442bf385a595ca0b88e710d71ae9820d0d04bf0c
Byron Bustamante López <42848404+bjoshua18@users.noreply.github.com>
2021-09-21 13:09:42 +0100

    Fix test db to remote (#6)

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,7 @@
 import { connect } from 'mongoose'
 
 export function startConnection() {
-  const uri = (process.env.ENV !== 'test')
-    ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gpjrd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-    : 'mongodb://localhost/reporter_api_test'
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gpjrd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   connect(uri)
     .catch(err => console.error(err.message))
 }
