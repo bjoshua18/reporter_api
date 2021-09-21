a852727b464d5dc7b333da71319bb35ff53ff531
Byron Bustamante <byronbl.12@gmail.com>
2021-09-21 12:21:48 +0100

    Change db from local to remote

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import { connect } from 'mongoose'
 
 export function startConnection() {
-  connect(process.env.DB_URI || '')
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gpjrd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  connect(uri)
     .catch(err => console.error(err.message))
 }
