0ad14bcde7ace4d07d7cc519a8a98d8533875afd
Byron Bustamante López <42848404+bjoshua18@users.noreply.github.com>
2021-09-21 12:46:54 +0100

    Change db from local to remote (#5)
    * Change db from local to remote
    
    * Change db to local for tests
    
    For tests, the database must be local because the timeout generates problems

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,9 @@
 import { connect } from 'mongoose'
 
 export function startConnection() {
-  connect(process.env.DB_URI || '')
+  const uri = (process.env.ENV !== 'test')
+    ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gpjrd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    : 'mongodb://localhost/reporter_api_test'
+  connect(uri)
     .catch(err => console.error(err.message))
 }
